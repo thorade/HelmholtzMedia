5ca7f36ca72da0eac1942853535e96eade894d19
thorade <matthis.thorade@gmail.com>
2013-02-04 11:26:37 +0100

    re-checking the BWR terms in frddd

--- Interfaces/PartialHelmholtzMedium/EoS/Helmholtz_energy_derivatives.m
+++ Interfaces/PartialHelmholtzMedium/EoS/Helmholtz_energy_derivatives.m
@@ -39,12 +39,12 @@ BWRTerms = bi1*tau^bi2*delta^bi3*exp(-delta^bi4);
 GaussTerms = gi1*tau^gi2*delta^gi3*exp(gi6*(delta-gi9)^2 + gi7*(tau-gi8)^2);
 nonAnalyticalTerms = 0;
 % fr = +residualPolyTerms +BWRTerms +GaussTerms +nonAnalyticalTerms;
-fr = +GaussTerms; 
+fr = +BWRTerms; 
 
 
 % First derivatives
 fit = diff(fi,tau);
-fit = simplify(fit)
+fit = simplify(fit);
 fid = diff(fi,delta);
 fid = simplify(fid);
 
@@ -56,7 +56,7 @@ frd = simplify(frd);
 
 % Second derivatives
 fitt = diff(fit,tau);
-fitt = simplify(fitt)
+fitt = simplify(fitt);
 
 frtt = diff(frt,tau);
 frtt = simplify(frtt);
@@ -68,13 +68,13 @@ frdd = simplify(frdd);
 
 % Third derivatives
 fittt = diff(fitt,tau);
-fittt = simplify(fittt)
+fittt = simplify(fittt);
 
 frttt = diff(frtt,tau);
-frttt = simplify(frttt)
+frttt = simplify(frttt);
 frttd = diff(frtd,tau);
 frttd = simplify(frttd);
 frtdd = diff(frtd,delta);
 frtdd = simplify(frtdd);
-frddd = diff(frdd,delta);
-frddd = simplify(frddd);
+frddd = diff(frdd,delta)
+frddd = simplify(frddd)
