0fd676efc4ecc5724fb7e19b7feaaac5cecce2fe
thorade <matthis.thorade@gmail.com>
2013-02-08 21:48:16 +0100

    #6 nonAnalyticalTerms now implemented in the Matlab script,
    saved the output (after simplyfication), it is quite lengthy.
    won't copy these terms to the Modelica code today

--- Interfaces/PartialHelmholtzMedium/EoS/Helmholtz_energy_derivatives.m
+++ Interfaces/PartialHelmholtzMedium/EoS/Helmholtz_energy_derivatives.m
@@ -1,7 +1,10 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Derivatives of the Helmholtz energy
 % Matthis Thorade, 2012
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% warning: simplifying the nonAnalyticalTerms will keep MatLab busy for a
+% while
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all;
 
@@ -23,7 +26,10 @@ syms residualPolyTerms BWRTerms GaussTerms nonAnalyticalTerms;
 
 % Parameters in the bank of terms
 syms li1 li2 ei1 ei2 ci1 ci2 si1 si2;
-syms pi1 pi2 pi3 bi1 bi2 bi3 bi4 gi1 gi2 gi3 gi4 gi5 gi6 gi7 gi8 gi9;
+syms pi1 pi2 pi3;
+syms bi1 bi2 bi3 bi4;
+syms gi1 gi2 gi3 gi4 gi5 gi6 gi7 gi8 gi9;
+syms ni1 ni2 ni3 ni4 ni5 ni6 ni7 ni8 ni9 ni10 ni11 ni12;
 
 % Terms for Helmholtz energy
 logTerms = + li1*log(tau^li2);
@@ -32,14 +38,17 @@ EinsteinTerms = + ei1*log(1 - exp(ei2*tau));
 coshTerms = - ci1*log((cosh(ci2*tau)));
 sinhTerms = + si1*log((sinh(si2*tau)));
 % fi = +log(delta) +logTerms +idealPolyTerms +EinsteinTerms +coshTerms +sinhTerms;
-fi = +sinhTerms;
+fi = +EinsteinTerms ;
+fi = simplify(fi)
 
 residualPolyTerms = pi1*tau^pi2*delta^pi3;
 BWRTerms = bi1*tau^bi2*delta^bi3*exp(-delta^bi4);
 GaussTerms = gi1*tau^gi2*delta^gi3*exp(gi6*(delta-gi9)^2 + gi7*(tau-gi8)^2);
-nonAnalyticalTerms = 0;
+Distance = ((1-tau)+ni8*((delta-1)^2)^(1/(2*ni7)))^2 + ni11*((delta-1)^2)^ni12
+nonAnalyticalTerms = ni1*Distance^ni6*delta*exp(-ni9*(delta-1)^2-ni10*(tau-1)^2);
 % fr = +residualPolyTerms +BWRTerms +GaussTerms +nonAnalyticalTerms;
-fr = +BWRTerms; 
+fr = +nonAnalyticalTerms  
+fr = simplify(fr)
 
 
 % First derivatives
@@ -49,9 +58,9 @@ fid = diff(fi,delta);
 fid = simplify(fid);
 
 frt = diff(fr,tau);
-frt = simplify(frt);
+frt = simplify(frt)
 frd = diff(fr,delta);
-frd = simplify(frd);
+frd = simplify(frd)
 
 
 % Second derivatives
@@ -59,11 +68,11 @@ fitt = diff(fit,tau);
 fitt = simplify(fitt);
 
 frtt = diff(frt,tau);
-frtt = simplify(frtt);
+frtt = simplify(frtt)
 frtd = diff(frt,delta);
-frtd = simplify(frtd);
+frtd = simplify(frtd)
 frdd = diff(frd,delta);
-frdd = simplify(frdd);
+frdd = simplify(frdd)
 
 
 % Third derivatives
@@ -71,10 +80,10 @@ fittt = diff(fitt,tau);
 fittt = simplify(fittt);
 
 frttt = diff(frtt,tau);
-frttt = simplify(frttt);
+frttt = simplify(frttt)
 frttd = diff(frtd,tau);
-frttd = simplify(frttd);
+frttd = simplify(frttd)
 frtdd = diff(frtd,delta);
-frtdd = simplify(frtdd);
-frddd = diff(frdd,delta)
+frtdd = simplify(frtdd)
+frddd = diff(frdd,delta);
 frddd = simplify(frddd)
