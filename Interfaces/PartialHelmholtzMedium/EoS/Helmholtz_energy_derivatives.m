8dbabf096fd43fb26c69a3f081c1e8565709cbc8
thorade <matthis.thorade@gmail.com>
2013-02-15 11:19:07 +0100

    use Matlab cell mode:
    percent percent space starts a new cell,
    each cell can be evaluated on its own.
    http://blogs.mathworks.com/community/2009/06/08/using-the-cell-mode-toolbar/

--- Interfaces/PartialHelmholtzMedium/EoS/Helmholtz_energy_derivatives.m
+++ Interfaces/PartialHelmholtzMedium/EoS/Helmholtz_energy_derivatives.m
@@ -1,9 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Derivatives of the Helmholtz energy
-% Matthis Thorade, 2012
-%
-% warning: simplifying the nonAnalyticalTerms will keep MatLab busy for a
-% while
+% Matthis Thorade, 2012%
+% warning: this might keep MatLab busy for a while
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all;
@@ -29,7 +27,7 @@ syms li1 li2 ei1 ei2 ci1 ci2 si1 si2;
 syms pi1 pi2 pi3;
 syms bi1 bi2 bi3 bi4;
 syms gi1 gi2 gi3 gi4 gi5 gi6 gi7 gi8 gi9;
-syms ni1 ni2 ni3 ni4 ni5 ni6 ni7 ni8 ni9 ni10 ni11 ni12;
+syms ni1 ni2 ni3 ni4 ni5 ni6 ni7 ni8 ni9 ni10 ni11 ni12 Distance Phi;
 
 % Terms for Helmholtz energy
 logTerms = + li1*log(tau^li2);
@@ -45,13 +43,13 @@ residualPolyTerms = pi1*tau^pi2*delta^pi3;
 BWRTerms = bi1*tau^bi2*delta^bi3*exp(-delta^bi4);
 GaussTerms = gi1*tau^gi2*delta^gi3*exp(gi6*(delta-gi9)^2 + gi7*(tau-gi8)^2);
 Distance = ((1-tau)+ni8*((delta-1)^2)^(1/(2*ni7)))^2 + ni11*((delta-1)^2)^ni12
-nonAnalyticalTerms = ni1*Distance^ni6*delta*exp(-ni9*(delta-1)^2-ni10*(tau-1)^2);
+Phi = exp(-ni9*(delta-1)^2-ni10*(tau-1)^2)
+nonAnalyticalTerms = ni1*delta*Distance^ni6*Phi;
 % fr = +residualPolyTerms +BWRTerms +GaussTerms +nonAnalyticalTerms;
 fr = +nonAnalyticalTerms  
 fr = simplify(fr)
 
-
-% First derivatives
+%% First derivatives
 fit = diff(fi,tau);
 fit = simplify(fit);
 fid = diff(fi,delta);
@@ -62,8 +60,7 @@ frt = simplify(frt)
 frd = diff(fr,delta);
 frd = simplify(frd)
 
-
-% Second derivatives
+%% Second derivatives
 fitt = diff(fit,tau);
 fitt = simplify(fitt);
 
@@ -74,8 +71,7 @@ frtd = simplify(frtd)
 frdd = diff(frd,delta);
 frdd = simplify(frdd)
 
-
-% Third derivatives
+%% Third derivatives
 fittt = diff(fitt,tau);
 fittt = simplify(fittt);
 
