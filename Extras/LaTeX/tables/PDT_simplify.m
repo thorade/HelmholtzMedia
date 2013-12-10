76ec2436f59725937d60f43347a115d6f9eb301f
thorade <matthis.thorade@gmail.com>
2013-12-10 15:06:56 +0100

    comment, didn't help

--- Extras/LaTeX/tables/PDT_simplify.m
+++ Extras/LaTeX/tables/PDT_simplify.m
@@ -1,7 +1,7 @@
 syms dP_dD_T dP_dT_D
-syms d2P_dD2_T d2P_dT2_D d2P_dDdT d2P_dTdD
+syms d2P_dD2_T d2P_dT2_D d2P_dDdT
 syms d2T_dD2_P
-d2P_dDdT = d2P_dTdD;
+%d2P_dDdT = d2P_dTdD;
 
 d2T_dD2_P = -(d2P_dD2_T*dP_dT_D - dP_dD_T*d2P_dDdT)/dP_dT_D^2 ...
             +(d2P_dTdD*dP_dT_D - dP_dD_T*d2P_dT2_D)*dP_dD_T/dP_dT_D^3
